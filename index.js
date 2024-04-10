32b6d8146324911e6183c0f950cffb3e33b838aa
AlvaroCasado99 <acasam01@estudiantes.unileon.es>
2024-04-10 18:12:58 +0200

    Possible fix: Only export app

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
 
-// const express = require('express')
-// const dotenv = require('dotenv')
-// const mongoose = require('mongoose')
-// const cors = require('cors')
+const express = require('express')
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const cors = require('cors')
 
-// const {connectDB} = require('./mongo')
-// const {notFound} = require('./middlewares/notFound.middleware')
+const {connectDB} = require('./mongo')
+const {notFound} = require('./middlewares/notFound.middleware')
 
-// const scheduleRouter = require('./routers/schedule.router').router
-// const userRouter = require('./routers/user.router').router
+const scheduleRouter = require('./routers/schedule.router').router
+const userRouter = require('./routers/user.router').router
 
-// // Express main application
-// const app = express()
+// Express main application
+const app = express()
 
-// // Me conecto a la base de datos de mongo (lo hago aquí porque simpre se carga este fichero al llegar una peticion)
-// connectDB()
+// Me conecto a la base de datos de mongo (lo hago aquí porque simpre se carga este fichero al llegar una peticion)
+connectDB()
 
-// //Middlewares
-// app.use(express.json())   // Body parser: poder recibir req con body JSON
-// app.use(cors())         // CrossOrigin...: necesario para el envio de datos JSON (entre otros)
+//Middlewares
+app.use(express.json())   // Body parser: poder recibir req con body JSON
+app.use(cors())         // CrossOrigin...: necesario para el envio de datos JSON (entre otros)
 
-// // Routers (Controllers)
-// app.use('/schedule', scheduleRouter)
+// Routers (Controllers)
+app.use('/schedule', scheduleRouter)
 
 
-// // End points
-// app.get("/", (req, res) => res.send("Express on Vercel"));
-
-// // 404 Error handler
-// app.use(notFound)
-
-// const PORT = process.env.PORT
-// const server = app.listen(PORT, () => {
-//     console.log(`Application is up. GO to http://localhost:${PORT}`)
-// })
-
-// module.exports = {
-//     app, 
-//     server
-// }
-
-const express = require("express");
-const app = express();
-
+// End points
 app.get("/", (req, res) => res.send("Express on Vercel"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+// 404 Error handler
+app.use(notFound)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Application is up. GO to http://localhost:${PORT}`)
+})
 
 module.exports = app;
